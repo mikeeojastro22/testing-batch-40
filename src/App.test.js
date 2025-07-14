d9ca37f9650223cbc435479cc1fa575870c8dd6f
Mikee Ojastro <mikeeojastro@gmail.com>
2025-07-14 20:42:47 +0800

    July 14, 2025

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// test(description, callback function)
 test('renders learn react link', () => {
+  // render - programatically renders the component as DOM tree
   render(<App />);
+
+  // screen - replica of the screen that we see
+  // getByText - find the element that contains the text
+  // /learn react/i - text to be matched/case insensitive
   const linkElement = screen.getByText(/learn react/i);
+
+  // Actual test
+  // expect - built in function that tests what's in the parameter
+  // linkElement - element to be tested
+  // toBeInTheDocument() - test/assert 
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,3 +1,44 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Counter from "./Counter";
+
+test("increment button works", () => {
+    // renders the Counter component
+    render(<Counter />);
+
+    // getting the counter
+    // binded data-testid="counter"
+    const counterValue = screen.getByTestId("counter");
+
+    // getting the increment button
+    const incrementButton = screen.getByText("+");
+
+    // actual test
+    // imitate the clicking of the increment button
+    // userEvent.click - used to "click" the button
+    userEvent.click(incrementButton);
+
+    // assert
+    // we need to typecast the textContent of counterValue
+    const counterText = Number(counterValue.textContent);
+    expect(counterText).toBeGreaterThan(0);
+});
+
+test("decrement button works", () => {
+    render(<Counter />);
+    
+    // getting the value of counter
+    const counterValue = screen.getByTestId("counter");
+
+    // imitate the clicking of the decrement button
+    // getting the element by data-testid
+    const decrementButton = screen.getByTestId("decrement");
+    // event to click the button
+    userEvent.click(decrementButton);
+
+    // actual test
+    // Typecasting the retrieved value to integer from the element
+    const counterText = Number(counterValue.textContent);
+    // assert if number is -1
+    expect(counterText).toBeLessThan(0);
+});
